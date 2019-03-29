83492850eb6f68c99639abfa63f04b3de0a4716f
UnknownMonk <smodcartoon@gmail.com>
2019-03-29 11:32:45 -0400

    dbConfig file set up and get routes working in index.js

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const db = require('./data/dbConfig.js');
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.get('/api/action', async (req, res) => {
+  try {
+    const roles = await db('action');
+    res.status(200).json(roles);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.get('/api/project', async (req, res) => {
+  try {
+    const roles = await db('project');
+    res.status(200).json(roles);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
